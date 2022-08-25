e24f3684db22301896cd1fbea4db0d3704a5113b
Daria <firefly88@mail.ru>
2022-08-26 02:20:07 +0600

    add Lesson1_Task4

---
+++ Lesson1_Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Enter number1:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number2:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number3:");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if((number1 > number2) && (number1 > number3))
+{
+    Console.WriteLine($"max = {number1}");
+}
+else if ((number2 > number1) && (number2 > number3))
+{
+    Console.WriteLine($"max = {number2}");
+}
+else
+{
+    Console.WriteLine($"max = {number3}");
+}
