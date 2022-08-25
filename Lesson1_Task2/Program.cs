8b8092bcdbf73bce2d84f0e6cb75e1b99c3adefa
Daria <firefly88@mail.ru>
2022-08-25 22:51:39 +0600

    add lesson 1 task 2

---
+++ Lesson1_Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter number1:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter number2:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1>number2)
+{
+    Console.WriteLine($"max = {number1}");
+}
+else
+{
+    Console.WriteLine($"max = {number2}");
+}
