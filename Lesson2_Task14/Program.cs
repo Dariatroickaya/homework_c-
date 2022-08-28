4774f921a0a2b4fbee66168276296190da70a73f
Daria <firefly88@mail.ru>
2022-08-29 02:28:36 +0600

    add Lesson2_Task14/Program

--- Lesson2_Task14/Program.cs
+++ Lesson2_Task14/Program.cs
@@ -1,16 +1,46 @@
 ﻿// Напишите программу, которая принимает на вход число и проверяет, 
 // кратно ли оно одновременно 7 и 23. 14 -> нет 46 -> нет 161 -> да
 
+// БЕЗ МЕТОДА
+// Console.WriteLine("Enter number:");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Enter number:");
-int number = Convert.ToInt32(Console.ReadLine());
+
+// if((number % 7 == 0) && (number % 23 == 0))
+// {
+//     Console.Write("YES");
+// }
+// else 
+// {
+//     Console.Write("NO");
+// }
 
 
-if((number % 7 == 0) && (number % 23 == 0))
+using System;
+
+namespace Seminar2
 {
-    Console.Write("YES");
-}
-else 
-{
-    Console.Write("NO");
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimultaneousMultiplicity();
+        }
+
+        static void SimultaneousMultiplicity()
+        {
+            Console.Write("Enter number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if((number % 7 == 0) && (number % 23 == 0))
+            {
+                Console.Write("YES");
+            }
+            else 
+            {
+                Console.Write("NO");
+            }
+        }
+
+    }
 }
