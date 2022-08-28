236a166b3b315ddafa0dcea0dc6881888e8bb12e
Daria <firefly88@mail.ru>
2022-08-29 03:24:35 +0600

    add Lesson2_Task15

---
+++ Lesson2_Task15/Program.cs
@@ -0,0 +1,62 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+//  и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+//Без метода
+
+// Console.Write("Enter number 1 to 7: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if((number > 0) && (number < 6))
+// {
+//     Console.WriteLine("НЕТ");
+// }
+// else if ((number > 5) && (number < 8))
+// {
+//     Console.WriteLine("ДА");
+// }
+// else
+// {
+//     Console.WriteLine("НЕВЕРНОЕ ЧИСЛО");
+// }
+
+
+
+
+
+using System;
+
+namespace Seminar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DayOfTheWeek();
+        }
+
+        static void DayOfTheWeek()
+        {
+          Console.Write("Enter number 1 to 7: ");
+          int number = Convert.ToInt32(Console.ReadLine());
+
+          if((number > 0) && (number < 6))
+            {
+              Console.WriteLine("НЕТ");
+            }
+          else if ((number > 5) && (number < 8))
+              {
+                Console.WriteLine("ДА");
+              }
+              else
+              {
+                Console.WriteLine("НЕВЕРНОЕ ЧИСЛО");
+              }
+        }
+
+    }
+}
