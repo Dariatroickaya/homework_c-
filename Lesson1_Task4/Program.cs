f9e57feeb767b0b5766774d54006ff99d5b347bf
Daria <firefly88@mail.ru>
2022-08-28 17:20:28 +0600

    Добавлено условие

--- Lesson1_Task4/Program.cs
+++ Lesson1_Task4/Program.cs
@@ -9,13 +9,21 @@ int number3 = Convert.ToInt32(Console.ReadLine());
 
 if((number1 > number2) && (number1 > number3))
 {
-    Console.WriteLine($"max = {number1}");
+    Console.WriteLine(number1);
 }
 else if ((number2 > number1) && (number2 > number3))
 {
-    Console.WriteLine($"max = {number2}");
+    Console.WriteLine(number2);
 }
 else
 {
-    Console.WriteLine($"max = {number3}");
+    Console.WriteLine(number3);
 }
+
+// Напишите программу, которая принимает на
+// вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine($"max = {number1}"); для вывода в виде "max = number"
