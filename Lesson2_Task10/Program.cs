b7e4f1c52e2e65ea8cc37fbecc48435db9f8d399
Daria <firefly88@mail.ru>
2022-08-29 02:49:09 +0600

    add Lesson2_Task10

---
+++ Lesson2_Task10/Program.cs
@@ -0,0 +1,68 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+//ВАРИАНТ 1
+
+// Console.WriteLine("Enter three-digit number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int number1 = number % 10;
+
+// int number2 = number-number1;
+
+// int result = (number2 % 100)/10;
+
+// Console.WriteLine(result);
+
+
+//ВАРИАНТ 2
+
+// int number = new Random().Next(100, 1000);
+
+// int number1 = number % 10;
+
+// int number2 = number-number1;
+
+// int result = (number2 % 100)/10;
+
+// Console.WriteLine(number);
+
+// Console.WriteLine(result);
+
+
+
+//ВАРИАНТ 3
+
+using System;
+
+namespace Seminar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindSecondNumber();
+        }
+
+        static void FindSecondNumber()
+        {
+           int number = new Random().Next(100, 1000);
+
+            int number1 = number % 10;
+
+            int number2 = number-number1;
+
+            int result = (number2 % 100)/10;
+
+            Console.WriteLine(number);
+
+            Console.WriteLine(result); 
+        }
+    }
+
+}
