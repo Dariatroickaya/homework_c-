125b9e31961b8ee6a4f02bc2e5713b05fcd7bcd8
Daria <firefly88@mail.ru>
2022-09-12 05:02:22 +0600

    add Lesson6_Task41

---
+++ Lesson6_Task41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3 (ПОЧЕМУ ТУТ 3?)
+
+
+Console.Write("Введите элементы массива через пробел: ");
+string elements = Console.ReadLine();
+
+int[] array = GetArrayFromString(elements);
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во чисел больше 0: {count}");
+
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
