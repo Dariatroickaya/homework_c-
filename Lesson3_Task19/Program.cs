d4bd3a7f74a2182b78ea3bf29e956620c80f406f
Daria <firefly88@mail.ru>
2022-09-11 18:43:31 +0600

    добавлено решение преподавателя

--- Lesson3_Task19/Program.cs
+++ Lesson3_Task19/Program.cs
@@ -48,3 +48,46 @@ namespace Lesson3
         }
     }
 }
+
+
+
+//РЕШЕНИЕ ПРЕПОДАВАТЕЛЯ
+
+//   public static bool IsPalindromeInt(int num)
+//         {
+//             // num = 456654
+//             int temp = num;
+//             // temp = 456654
+//             int revert = 0;
+
+//             while (temp > 0)
+//             {
+//                 //revert= 0*10=0 + 4=4
+//                  //revert= 4*10=40 + 5=45
+//                  //revert= 45*10=450 + 6=456
+//                  //revert= 456*10=4560 + 6=4566
+//                  //revert= 4566*10=45660 +5=45665
+//                  //revert= 45665*10=456650 + 6=456654
+//                 revert = (revert * 10) + (temp % 10); 
+//                 //temp = 45665
+//                 //temp = 456
+//                 //temp = 45
+//                 //temp = 4
+//                 temp /= 10;
+//             }
+
+//             return revert == num;
+//         }
+
+//         public static bool IsPalindromeString(string str)
+//         {
+//             int size = str.Length;
+//             for (int i = 0; i <= size / 2; ++i)
+//             {
+//                 if (str[i] != str[size - 1 - i])
+//                 {
+//                     return false;
+//                 }
+//             }
+//             return true;
+//         }
