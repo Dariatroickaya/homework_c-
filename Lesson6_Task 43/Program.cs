c6549507888df2f6c70be0195272087adccbad39
Daria <firefly88@mail.ru>
2022-09-12 04:34:14 +0600

    add Lesson6_Task43

--- Lesson6_Task 43/Program.cs
+++ Lesson6_Task 43/Program.cs
@@ -22,13 +22,15 @@
 
 Console.Write("Введите b1: ");
 var b1 = Convert.ToDouble(Console.ReadLine());
+
 Console.Write("Введите k1: ");
 var k1 = Convert.ToDouble(Console.ReadLine());
+
 Console.Write("Введите b2: ");
 var b2 = Convert.ToDouble(Console.ReadLine());
+
 Console.Write("Введите k2: ");
 var k2 = Convert.ToDouble(Console.ReadLine());
- 
  
 var x = (b2 - b1) / (k1 - k2);
 var y = k2 * x + b2;
