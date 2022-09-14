5585aff70c3ee45c7ea683a43d1b08d3bb2db71c
Daria <firefly88@mail.ru>
2022-09-14 18:40:42 +0600

    add Lesson4_Task27

--- Lesson4_Task27/Program.cs
+++ Lesson4_Task27/Program.cs
@@ -26,3 +26,26 @@ int SumOfNumbers(int number)
    return result;
   }
 
+
+//РЕШЕНИЕ ПРЕПОДАВАТЕЛЯ
+// void SumNumbers()
+// {
+//     Console.Clear();
+
+//     int num = SetNumber("N");
+//     Console.WriteLine($"Суммф цифр = {GetSumNums(num)}");
+// }
+
+
+// int GetSumNums(int number)
+// {
+//     int sum = 0;
+
+//     while (number > 0)
+//     {
+//         sum += number % 10;
+//         number /= 10;
+//     }
+
+//     return sum;
+// }
