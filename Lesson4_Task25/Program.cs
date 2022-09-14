887d2fb97239a19d510156cf4ffe7a60a216f0c7
Daria <firefly88@mail.ru>
2022-09-14 18:23:53 +0600

    add Lesson4_Task25

--- Lesson4_Task25/Program.cs
+++ Lesson4_Task25/Program.cs
@@ -42,4 +42,35 @@ int NumberToThePower(int numberA, int numberB)
         return result;
     }
 
-  
+
+
+//РЕШЕНИЕ ПРЕПОДАВАТЕЛЯ
+
+//   void MyPow()
+// {
+
+//     Console.Clear();
+
+//     int num = SetNumber("");
+
+//     int degree = SetNumber("degree of");
+
+//     Console.WriteLine($"{num} в степени {degree} = {Pow(num, degree)}");
+// }
+
+
+
+// int Pow(int num, int rank)
+// {
+//     if (rank == 0) return 1;
+//     else if (rank == 1) return num;
+
+//     int result = num;
+
+//     for (int i = 2; i <= rank; i++)
+//     {
+//         result *= num;
+//     }
+
+//     return result;
+// }
