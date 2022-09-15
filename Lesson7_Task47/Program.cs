bc72e72d0bb7b243a0be28f0e5788913f125c9aa
Daria <firefly88@mail.ru>
2022-09-15 22:31:44 +0600

    add Lesson7_Task47

---
+++ Lesson7_Task47/Program.cs
@@ -0,0 +1,84 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными 
+// вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5    7    -2    -0,2
+ 
+// 1    -3,3    8    -9,9
+
+// 8     7,8  -7,1     9
+
+
+// Console.Write("Введите количество строк массива: ");
+// int rows = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите количество столбцов массива: ");
+// int columns = int.Parse(Console.ReadLine());
+
+// int[,] array = FillArray(rows, columns, 0, 9);
+// PrintArray(array);
+
+// int[,] FillArray(int rows, int columns, int minValue, int maxValue)
+// {
+//     int[,] result = new int[rows, columns];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//             result[i, j] = new Random().Next(minValue, maxValue + 1);
+//         }
+//     }
+//     return result;
+// }
+
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write($"{array[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = FillArray(rows, columns);
+PrintArray(array);
+
+double[,] FillArray(int rows, int columns)
+{
+    double[,] result = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = new Random().NextDouble()*10;
+        }
+    }
+    return result;
+}
+
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]:f2} ");
+        }
+        Console.WriteLine();
+    }
+}
+
