3a7a4adc27b943a4ed113ff5b4399a7c71438d09
Daria <firefly88@mail.ru>
2022-09-19 19:35:20 +0600

    исправлена матрица

--- Lesson7_Task52/Program.cs
+++ Lesson7_Task52/Program.cs
@@ -63,15 +63,15 @@
         {
             int[,] array = new int[3,4] {{1,4,7,2},{5,9,2,3},{8,4,2,4}};
  
-            for (int i = 0; i < 3; i++) 
+            for (int j = 0; j < 4; j++) 
             {
                 int average = 0;
-                for (int j = 0; j < 4; j++)
+                for (int i = 0; i < 3; i++)
                 {
                     average += array[i, j];
                     
                 }
-                Console.WriteLine($"Cреднее арифметическое элементов столбца {i + 1} = {(float)average/3}");
+                Console.WriteLine($"Cреднее арифметическое элементов столбца {j + 1} = {(float)average/3}");
                 
             }
         }
