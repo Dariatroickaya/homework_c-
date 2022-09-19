df1e392b5ef3f4198e342542d342d9844f5a35b1
Daria <firefly88@mail.ru>
2022-09-19 20:02:44 +0600

    добавлено решение

--- Lesson7_Task50/Program.cs
+++ Lesson7_Task50/Program.cs
@@ -52,7 +52,70 @@ if (indexrows > rows || indexcolumns > columns)
 }
 else
 {
-    //object find = array.GetValue(indexrows,indexcolumns);  // в этом месте мне тоже помогли. буду ждать вашего решения и обьяснения на семинаре
+    //object find = array.GetValue(indexrows,indexcolumns);  
     //Console.WriteLine(find);
     Console.WriteLine(array[indexrows,indexcolumns]);
 }
+
+
+//РЕШЕНИЕ ПРЕПОДАВАТЕЛЯ
+// Console.Clear();
+
+// Console.Write("Введите количество строк массива: ");
+//  rows=int.Parse(Console.ReadLine());
+
+// Console.Write("Введите количество столбцов массива: ");
+//  columns=int.Parse(Console.ReadLine());
+
+
+// int[,] array = GetArray(rows, columns,0,10);
+// PrintArray(array);
+// Console.Write("Введите значение элемента: ");
+// int element=int.Parse(Console.ReadLine());
+// if(FindElement(array,element))
+// {
+//     Console.WriteLine("Элемент найден");
+// }
+// else
+// {
+//     Console.WriteLine("Элемент не найден");
+// }
+
+// int[,] GetArray(int m, int n, int min, int max)
+// {
+//     int[,] result = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().Next(min, max + 1);
+//         }
+
+//     }
+//     return result;
+// }
+
+// void PrintArray(int[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{inArray[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+
+// bool FindElement(int[,] array, int el)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if (array[i, j]==el) return true;
+//         }
+//     }
+//     return false;
+// }
