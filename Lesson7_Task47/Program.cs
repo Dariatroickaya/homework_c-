5429991a8d6c8215b773e453d94ba10734bf411a
Daria <firefly88@mail.ru>
2022-09-19 20:01:21 +0600

    добавлено решение

--- Lesson7_Task47/Program.cs
+++ Lesson7_Task47/Program.cs
@@ -10,43 +10,6 @@
 // 8     7,8  -7,1     9
 
 
-// Console.Write("Введите количество строк массива: ");
-// int rows = int.Parse(Console.ReadLine());
-
-// Console.Write("Введите количество столбцов массива: ");
-// int columns = int.Parse(Console.ReadLine());
-
-// int[,] array = FillArray(rows, columns, 0, 9);
-// PrintArray(array);
-
-// int[,] FillArray(int rows, int columns, int minValue, int maxValue)
-// {
-//     int[,] result = new int[rows, columns];
-//     for (int i = 0; i < rows; i++)
-//     {
-//         for (int j = 0; j < columns; j++)
-//         {
-//             result[i, j] = new Random().Next(minValue, maxValue + 1);
-//         }
-//     }
-//     return result;
-// }
-
-
-// void PrintArray(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             Console.Write($"{array[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
-
-
-
 Console.Write("Введите количество строк массива: ");
 int rows = Convert.ToInt32(Console.ReadLine());
 
@@ -82,3 +45,44 @@ void PrintArray(double[,] array)
     }
 }
 
+
+
+
+
+
+//РЕШЕНИЕ ПРЕПОДАВАТЕЛЯ
+// Console.Clear();
+
+// Console.Write("Введите количество строк массива: ");
+// int rows=int.Parse(Console.ReadLine());
+
+// Console.Write("Введите количество столбцов массива: ");
+// int columns=int.Parse(Console.ReadLine());
+
+// double[,] arrayDouble = GetArrayDouble(rows, columns, 0, 10);
+// PrintArrayDouble(arrayDouble);
+
+// double[,] GetArrayDouble(int m, int n, int min, int max)
+// {
+//     double[,] result = new double[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             result[i, j] = new Random().NextDouble()*(max-min);
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintArrayDouble(double[,] inArray)
+// {
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{inArray[i,j]:f1} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
